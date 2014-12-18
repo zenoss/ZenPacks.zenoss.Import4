405e8ecd2984b623e33b33f76c4de2b1bae3e032
Alex Chow <achow@zenoss.com>
2014-12-18 14:29:39 -0600

    Further cleanup for TA23650 migration skeleton. - a mock-up event migration class is included - further cosmetic clean up for import4 code

--- ZenPacks/zenoss/Import4/migration.py
+++ ZenPacks/zenoss/Import4/migration.py
@@ -12,32 +12,40 @@ exitCode = {
     None: -1    # when the method has not been implemented
 }
 
-class MigrationBase(object):
-    def __NOT_YET__(self):
-        print "%s:%s is not implemented yet" % \
-            (self.__class__.__name__, inspect.stack()[1][3])
-        # raise NotImplementedError
-        return
 
+class MigrationBase(object):
     def __init__(self, args, progressCallback):
         self.args = args
         self.progress = progressCallback
 
+    def __NOT_YET__(self):
+        caller = inspect.stack()[1]
+        print "-- %s:%s:%s not implemented! --" % \
+            (inspect.getmodule(caller[0]).__name__,
+             self.__class__.__name__, caller[3])
+        # raise NotImplementedError
+        return
+
     def prevalidate(self):
         self.__NOT_YET__()
+        print "-- To be overriden!"
         return
 
     def wipe(self):
         self.__NOT_YET__()
+        print "-- To be overriden!"
         return
 
     def doImport(self):
         self.__NOT_YET__()
+        print "-- To be overriden!"
         return
 
     def reportProgress(self, progress):
         self.progress(progress)
+        print "-- To be overriden!"
 
     def postvalidate(self):
         self.__NOT_YET__()
+        print "-- To be overriden!"
         return
