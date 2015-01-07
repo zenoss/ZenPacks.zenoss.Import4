c76b62327f1f9ed7ce6d037b33848e3e1aab5029
Alex Chow <achow@zenoss.com>
2015-01-07 10:07:31 -0600

    Add copyright statements

--- ZenPacks/zenoss/Import4/events.py
+++ ZenPacks/zenoss/Import4/events.py
@@ -1,3 +1,12 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2014-2015, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
 import os
 import subprocess
 import tempfile
--- ZenPacks/zenoss/Import4/interfaces.py
+++ ZenPacks/zenoss/Import4/interfaces.py
@@ -1,3 +1,12 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2014-2015, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
 # ZuulMessageFactory is the translation layer. You will see strings intended to
 # been seen in the web interface wrapped in _t(). This is so that these strings
 # can be automatically translated to other languages.
