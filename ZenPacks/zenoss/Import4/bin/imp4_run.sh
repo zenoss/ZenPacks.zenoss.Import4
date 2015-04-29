043203ff0957b6b0038c885f4fa34e28555d594c
Alex Chow <achow@zenoss.com>
2015-04-29 11:39:23 -0500

    Update imp4_run.sh
    Per review comments.

--- ZenPacks/zenoss/Import4/bin/imp4_run.sh
+++ ZenPacks/zenoss/Import4/bin/imp4_run.sh
@@ -10,14 +10,13 @@
 
 # Parse the arguments
 
-DESIRED_USER=${DESIRED_USER:-"zenoss"}
 # temporary line to copy /import4 from development code to the zenpack egg image
 cmd="cp -rp /import4/pkg/* /opt/zenoss/ZenPacks/ZenPacks.zenoss.Import4-0.0.9dev-py2.7.egg/ZenPacks/zenoss/Import4"
-su - "$DESIRED_USER" -c "$cmd"
+su - zenoss -c "$cmd"
 
 # trace_opt='-m trace --trace --ignore-module=$(cat /mnt/dos/ignore.lst)'
 trace_opt=''
 
 # use the mounted directory as the current directory
-cmd="DESIRED_USER=$DESIRED_USER cd /mnt/pwd; /opt/zenoss/bin/python $trace_opt /import4/pkg/bin/import4 $*"
-su - "$DESIRED_USER" -c "$cmd"
+cmd="cd /mnt/pwd; /opt/zenoss/bin/python $trace_opt /import4/pkg/bin/import4 $*"
+su - zenoss -c "$cmd"
