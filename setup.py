9d92e810753c7c1b860ff3fb38fae8a2f5c1948e
Chet Luther <chet.luther@gmail.com>
2015-11-11 18:22:34 +0000

    Start 1.1.0 dev: Version 1.0.0 -> 1.1.0dev.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.Import4"
-VERSION = "1.0.0"
+VERSION = "1.1.0dev"
 AUTHOR = "Zenoss"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
