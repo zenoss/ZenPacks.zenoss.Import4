369adde724cf6e8c535dc379b0e3b604c72be932
Alex Chow <achow@zenoss.com>
2015-11-12 14:36:05 -0600

    Update imp4_run.sh
    Make it polling longer for local mysqld

--- ZenPacks/zenoss/Import4/bin/imp4_run.sh
+++ ZenPacks/zenoss/Import4/bin/imp4_run.sh
@@ -30,7 +30,7 @@ prep_mysqld()
     while ! /usr/bin/mysqladmin --socket="$mysql_sock" status >/dev/null 2>&1
     do
         echo -n '.'
-        sleep 15 
+        sleep 20
         ((i=i+1))
         ((i>12)) && echo -en "\nCannot start mysql daemon!" && exit 1
     done
